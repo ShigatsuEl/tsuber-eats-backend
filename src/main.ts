9221a25e7853860a3968889c9383bec97ffbc2b5
MinChan Lee <shigatsu970704@gmail.com>
2021-04-02 15:57:07 +0900

    06.Nest GraphQL Valiate ArgumentType

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  // AppMoudle은 main.ts로 import되는 유일한 모듈이다.
+  // 그렇기 때문에 나중에 모든 것을 Appmoudle로 import될 것이다.
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+  await app.listen(3000);
+}
+bootstrap();
