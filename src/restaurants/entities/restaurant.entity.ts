f589a465b9152b598a20fcef4874000908201810
MinChan Lee <shigatsu970704@gmail.com>
2021-04-08 16:10:12 +0900

    15.Mapped Types

--- src/restaurants/entities/restaurant.entity.ts
+++ src/restaurants/entities/restaurant.entity.ts
@@ -1,9 +1,10 @@
 import { Field, ObjectType } from '@nestjs/graphql';
+import { IsBoolean, IsString, Length } from 'class-validator';
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
-// GraphQL을 위한 ObjectType
+// GraphQL을 위한 ObjectType / Field
+// TypeORM을 위한 Entity / Column
 @ObjectType()
-// TypeORM을 위한 Entity
 @Entity()
 export class Restaurant {
   @PrimaryGeneratedColumn()
@@ -12,21 +13,27 @@ export class Restaurant {
 
   @Field((type) => String)
   @Column()
+  @IsString()
+  @Length(5)
   name: string;
 
   @Field((type) => Boolean)
   @Column()
+  @IsBoolean()
   isVegan: boolean;
 
   @Field((type) => String)
   @Column()
+  @IsString()
   address: string;
 
   @Field((type) => String)
   @Column()
+  @IsString()
   ownerName: string;
 
   @Field((type) => String)
   @Column()
+  @IsString()
   categoryName: string;
 }
