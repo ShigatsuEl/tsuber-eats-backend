9cab2809db2531063396da444a773c1a0a5702f7
MinChan Lee <shigatsu970704@gmail.com>
2021-04-09 13:55:12 +0900

    21.Create Account Mutation(Input & Output Dto)

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,16 +1,22 @@
+import { Field, InputType, ObjectType } from '@nestjs/graphql';
 import { Core } from 'src/common/entities/core.entity';
 import { Column, Entity } from 'typeorm';
 
 type UserRole = 'client' | 'owner' | 'delivery';
 
+@InputType({ isAbstract: true })
+@ObjectType()
 @Entity()
 export class User extends Core {
+  @Field((type) => String)
   @Column()
   email: string;
 
+  @Field((type) => String)
   @Column()
   password: string;
 
+  @Field((type) => String)
   @Column()
   role: UserRole;
 }
