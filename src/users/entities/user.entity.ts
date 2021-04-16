2b7517fb93f04f2448c05c5300f7513ac767d5f5
MinChan Lee <shigatsu970704@gmail.com>
2021-04-16 11:11:53 +0900

    43.Verification Entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -36,6 +36,10 @@ export class User extends Core {
   @IsEnum(UserRole)
   role: UserRole;
 
+  @Column({ default: false })
+  @Field((type) => Boolean)
+  verified: boolean;
+
   @BeforeInsert()
   @BeforeUpdate()
   async hashPassword(): Promise<void> {
