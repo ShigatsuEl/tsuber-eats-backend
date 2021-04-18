2817f434d63740d4215e1f305bbaccbe06270900
MinChan Lee <shigatsu970704@gmail.com>
2021-04-18 17:10:58 +0900

    51.MailGun Template

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -9,15 +9,17 @@ export class MailService {
   constructor(
     @Inject(CONFIG_OPTIONS) private readonly options: MailModuleOptions,
   ) {
-    this.sendEmail('test', 'testing');
+    this.sendEmail('test', 'tsuber-eats-template');
   }
 
-  private async sendEmail(subject: string, content: string) {
+  private async sendEmail(subject: string, template: string) {
     const form = new FormData();
     form.append('from', `Excited User <mailgun@${this.options.domain}>`);
     form.append('to', `shigatsu970704@gmail.com`);
     form.append('subject', subject);
-    form.append('text', content);
+    form.append('template', template);
+    form.append('v:username', 'Tsuel');
+    form.append('v:code', 'abcd');
     const response = await got(
       `https://api.mailgun.net/v3/${this.options.domain}/messages`,
       {
