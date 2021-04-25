ab731c15690836c301fab7177e737baa74440ee2
MinChan Lee <shigatsu970704@gmail.com>
2021-04-25 11:12:49 +0900

    76.E2E Test CreateAccount(Should Fail If Account Already Exists)

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -6,6 +6,12 @@ import { getConnection } from 'typeorm';
 
 const GRAPHQL_ENDPOINT = '/graphql';
 
+jest.mock('got', () => {
+  return {
+    post: jest.fn(),
+  };
+});
+
 describe('UserModule (e2e)', () => {
   let app: INestApplication;
 
@@ -49,7 +55,31 @@ describe('UserModule (e2e)', () => {
         });
     });
 
-    it.todo('should fail if account already exists');
+    it('should fail if account already exists', () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .send({
+          query: `
+          mutation {
+            createAccount(input: {
+              email: "${EMAIL}",
+              password: "kimiuso",
+              role: Owner
+            }) {
+            ok
+            error
+          }
+        }
+        `,
+        })
+        .expect(200)
+        .expect((res) => {
+          expect(res.body.data.createAccount.ok).toBe(false);
+          expect(res.body.data.createAccount.error).toBe(
+            'There is a user with that email already',
+          );
+        });
+    });
   });
 
   it.todo('getUserProfile');
