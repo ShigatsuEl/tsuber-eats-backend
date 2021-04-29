d882080371c245d1244eddb1a9c12be89a3265e8
MinChan Lee <shigatsu970704@gmail.com>
2021-04-29 11:51:34 +0900

    90.Edit Restaurant(Resolver)

--- README.md
+++ README.md
@@ -26,3 +26,17 @@ The Backend of Tsuber Eats
 - See Profile
 - Edit Profile
 - Verify Email
+
+## Restaurant CRUD
+
+- See Categories
+- See Restaurants by Category (pagination)
+- See Restaurants (pagination)
+- See Restaurants
+
+- Edit Restaurant
+- Delete Restaurant
+
+- Create Dish
+- Edit Dish
+- Delete Dish
--- src/restaurants/restaurants.service.ts
+++ src/restaurants/restaurants.service.ts
@@ -6,6 +6,10 @@ import {
   CreateRestaurantInput,
   CreateRestaurantOutput,
 } from './dtos/create-restaurant.dto';
+import {
+  EditRestaurantInput,
+  EditRestaurantOutput,
+} from './dtos/edit-restaurant.dto';
 import { Category } from './entities/category.entity';
 import { Restaurant } from './entities/restaurant.entity';
 
@@ -45,4 +49,9 @@ export class RestaurantService {
       return { ok: false, error: 'Could not create restaurant' };
     }
   }
+
+  async editRestaurant(
+    owner: User,
+    editRestaurantInput: EditRestaurantInput,
+  ): Promise<EditRestaurantOutput> {}
 }
