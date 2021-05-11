0dfdf84cdc60871ba6257a06ade9d01e20a622d9
MinChan Lee <shigatsu970704@gmail.com>
2021-05-11 13:54:55 +0900

    115.Create Order(Service Part Five)

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -28,37 +28,48 @@ export class OrderService {
     try {
       const restaurant = await this.restaurants.findOne(restaurantId);
       if (!restaurant) return { ok: false, error: 'Restaurant not found' };
+      const orderItems: OrderItem[] = [];
+      let orderTotalPrice = 0;
+      // Dto를 통해 받아온 items로부터 orderItem을 생성하고 order을 생성한다.
       for (const item of items) {
         const dish = await this.dishes.findOne(item.dishId);
+        let dishTotalPrice = dish.price;
         if (!dish) return { ok: false, error: 'Dish not found' };
-        console.log(`Dish price: ${dish.price}`);
         for (const itemOption of item.options) {
           const dishOption = dish.options.find(
             (dishOption) => dishOption.name === itemOption.name,
           );
           if (dishOption) {
             if (dishOption.extra) {
-              console.log(`KRW: ${dishOption.extra}`);
+              dishTotalPrice += dishOption.extra;
             } else {
               const dishOptionChoice = dishOption.choices.find(
                 (optionChoice) => optionChoice.name === itemOption.choice,
               );
               if (dishOptionChoice && dishOptionChoice.extra) {
-                console.log(`KRW: ${dishOptionChoice.extra}`);
+                dishTotalPrice += dishOptionChoice.extra;
               }
             }
           }
         }
-        /* await this.orderItems.save(
+        orderTotalPrice += dishTotalPrice;
+        const orderItem = await this.orderItems.save(
           this.orderItems.create({
             dish,
             options: item.options,
           }),
-        ); */
+        );
+        orderItems.push(orderItem);
       }
-      /*  const order = await this.orders.save(
-        this.orders.create({ customer, restaurant }),
-      ); */
+      await this.orders.save(
+        this.orders.create({
+          customer,
+          restaurant,
+          total: orderTotalPrice,
+          items: orderItems,
+        }),
+      );
+      return { ok: true };
     } catch (error) {
       return { ok: false, error: 'Can not create order' };
     }
