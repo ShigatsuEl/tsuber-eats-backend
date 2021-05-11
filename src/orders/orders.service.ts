9337bf6e65bccbd9fd488ae5d1e570c04a566d0e
MinChan Lee <shigatsu970704@gmail.com>
2021-05-11 12:22:29 +0900

    114.Create Order(Service Part Three)

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { Dish } from 'src/restaurants/entities/dish.entity';
 import { Restaurant } from 'src/restaurants/entities/restaurant.entity';
 import { User } from 'src/users/entities/user.entity';
 import { Repository } from 'typeorm';
 import { CreateOrderInput, CreateOrderOutput } from './dtos/create-order.dto';
+import { OrderItem } from './entities/order-item.entity';
 import { Order } from './entities/order.entity';
 
 @Injectable()
@@ -11,6 +13,10 @@ export class OrderService {
   constructor(
     @InjectRepository(Order)
     private readonly orders: Repository<Order>,
+    @InjectRepository(OrderItem)
+    private readonly orderItems: Repository<OrderItem>,
+    @InjectRepository(Dish)
+    private readonly dishes: Repository<Dish>,
     @InjectRepository(Restaurant)
     private readonly restaurants: Repository<Restaurant>,
   ) {}
@@ -22,10 +28,21 @@ export class OrderService {
     try {
       const restaurant = await this.restaurants.findOne(restaurantId);
       if (!restaurant) return { ok: false, error: 'Restaurant not found' };
-      const order = await this.orders.save(
+      items.forEach(async (item) => {
+        const dish = await this.dishes.findOne(item.dishId);
+        if (!dish) {
+          // abort this whole thing
+        }
+        await this.orderItems.save(
+          this.orderItems.create({
+            dish,
+            options: item.options,
+          }),
+        );
+      });
+      /*  const order = await this.orders.save(
         this.orders.create({ customer, restaurant }),
-      );
-      console.log(order);
+      ); */
     } catch (error) {
       return { ok: false, error: 'Can not create order' };
     }
