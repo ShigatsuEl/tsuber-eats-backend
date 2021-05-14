d6442bd8519a1694640eb79a0ca4ee6494845005
MinChan Lee <shigatsu970704@gmail.com>
2021-05-14 12:15:30 +0900

    129.Cooked Order

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,7 +1,11 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { PubSub } from 'graphql-subscriptions';
-import { NEW_PENDING_ORDER, PUB_SUB } from 'src/common/common.constants';
+import {
+  NEW_COOKED_ORDER,
+  NEW_PENDING_ORDER,
+  PUB_SUB,
+} from 'src/common/common.constants';
 import { Dish } from 'src/restaurants/entities/dish.entity';
 import { Restaurant } from 'src/restaurants/entities/restaurant.entity';
 import { User, UserRole } from 'src/users/entities/user.entity';
@@ -181,7 +185,14 @@ export class OrderService {
           ok: false,
           error: 'You do not have permission or can not edit',
         };
-      await this.orders.save([{ id: orderId, status }]);
+      await this.orders.save({ id: orderId, status });
+      if (user.role === UserRole.Owner) {
+        if (status === OrderStatus.Cooked) {
+          this.pubSub.publish(NEW_COOKED_ORDER, {
+            cookedOrders: { ...order, status },
+          });
+        }
+      }
       return { ok: true };
     } catch (error) {
       return { ok: false, error: 'Could not edit order' };
