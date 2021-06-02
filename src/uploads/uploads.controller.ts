922270bb18eae97bb31093f394031ce95da0ce81
MinChan Lee <shigatsu970704@gmail.com>
2021-06-02 17:06:57 +0900

    143.File Upload(AWS S3)

--- src/uploads/uploads.controller.ts
+++ src/uploads/uploads.controller.ts
@@ -5,12 +5,35 @@ import {
   UseInterceptors,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
+import * as AWS from 'aws-sdk';
+
+const BUCKET_NAME = 'tsuber-eats-bucket';
 
 @Controller('uploads')
 export class UploadController {
   @Post('')
   @UseInterceptors(FileInterceptor('file'))
-  uploadFile(@UploadedFile() file: Express.Multer.File) {
-    console.log(file);
+  async uploadFile(@UploadedFile() file: Express.Multer.File) {
+    AWS.config.update({
+      credentials: {
+        accessKeyId: process.env.AWS_ID,
+        secretAccessKey: process.env.AWS_SECRET,
+      },
+    });
+    try {
+      const objectName = `${Date.now() + file.originalname}`;
+      await new AWS.S3()
+        .putObject({
+          Body: file.buffer,
+          Bucket: BUCKET_NAME,
+          Key: objectName,
+          ACL: 'public-read',
+        })
+        .promise();
+      const url = `https://${BUCKET_NAME}.s3.amazonaws.com/${objectName}`;
+      return { url };
+    } catch (error) {
+      return null;
+    }
   }
 }
